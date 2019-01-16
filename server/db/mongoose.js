58bda7686f923e1c53feaa36f867118b7f0630d9
PicNer <pic.ner101@gmail.com>
2019-01-16 22:25:24 +0300

    any

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/anytable')
+mongoose.connect('mongodb://localhost:27017/todosTable')
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
